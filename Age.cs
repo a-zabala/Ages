183b6341a6d87a55441a826f7100c29ef99a5a71
Angelina Zabala <angekr@aol.com>
2017-12-13 19:03:34 -0600

    random

---
+++ Age.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your birthday?");
+            var birthDate = Console.ReadLine();
+            var start1 = DateTime.Parse(birthDate);
+            var end =  DateTime.Today - start1;
+            var years = end.TotalDays / 365;
+            Console.WriteLine(years);
+            Console.ReadLine();        }
+    }
+}
